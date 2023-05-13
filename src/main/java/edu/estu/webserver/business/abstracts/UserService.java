db6b357850d57fae20a8d78ef6500ad17a51ebe4
EmreCanCakir <emreecann28@gmail.com>
2023-05-13 22:34:49 +0300

    delete userservice and manager

--- src/main/java/edu/estu/webserver/business/abstracts/UserService.java
+++
@@ -1,7 +0,0 @@
-package edu.estu.webserver.business.abstracts;
-
-import edu.estu.webserver.core.business.BaseService;
-import edu.estu.webserver.entities.User;
-
-public interface UserService extends BaseService<User> {
-}
