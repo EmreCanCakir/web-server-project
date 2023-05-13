562c0d665ec9987b062956879f38c83341c03f5a
EmreCanCakir <emreecann28@gmail.com>
2023-05-13 20:14:21 +0300

    add result structure

---
+++ src/main/java/edu/estu/webserver/core/utilities/results/SuccessResult.java
@@ -0,0 +1,12 @@
+package edu.estu.webserver.core.utilities.results;
+
+public class SuccessResult extends Result {
+    public SuccessResult() {
+        super(true);
+    }
+
+    public SuccessResult(String message) {
+        super(true,message);
+    }
+
+}
